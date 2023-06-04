bb49b65037a5fa6b2550890a220ecd4feeba93ce
池振训 <zeschi@kugou.net>
2023-06-04 23:15:44 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+st.write(1234)
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
